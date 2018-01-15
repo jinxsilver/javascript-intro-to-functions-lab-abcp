3ff6430373c0bfd59f84130e51d0e5262a2ecd87
rey soto <jinxsilver@gmail.com>
2018-01-15 02:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma() {
-  return 'I can\'t hear you!'
-}
+  if (string.toLowerCase() ===) {
+      return "I can\'t hear you!"
+  } else if (string.toUpperCase() === string){
+    return "Yes INDEED!"
+  } else if (string === "I Love you, Grandma."){
+    return "I love you, too.";
+  }
+};
